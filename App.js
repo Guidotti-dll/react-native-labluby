4a627b1847cf53deb898457c42305fc9f9ce014a
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-29 18:54:07 -0300

    feat: request api

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+import {WEATHER_API_KEY} from '@env'
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [errorMessage, setErrorMessage] = useState('')
+  const [currentWeather, setCurrentWeather] = useState(null)
+
+  
+  useEffect(( ) => {
+    load()
+  }, [])
+
+  const load = async () => {
+    try {
+      let {status} = await Location.requestBackgroundPermissionsAsync()
+
+      if(status !== 'granted'){
+        setErrorMessage('Access to location is needed to run the app')
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync()
+      const {latitude, longitude} = location.coords;
+
+      const url = `${BASE_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+      const response = await fetch(url)
+      const result = await response.json()
+      if(response.ok){
+        setCurrentWeather(result)
+      }else{
+        setErrorMessage(result.message)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if(currentWeather){
+    const {main: {temp}} = currentWeather
+    return (
+      <View style={styles.container}>
+        <Text>{temp}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }else{
+    return(
+      <View style={styles.container}>
+        <Text>{errorMessage}aaaaaa</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
